9c360770aad5282161bb609662e0d1b0b71fc3c0
Hamed <hamedullah@gmail.com>
2021-03-02 10:29:28 +0200

    Edite the Model part and create function to get the Data.

--- ShoppinList/Model/ProductModel.swift
+++ ShoppinList/Model/ProductModel.swift
@@ -6,54 +6,32 @@
 //
 
 import Foundation
-
 struct ProductModel: Codable {
-    let store: [Store]
-     enum CodingKeys: String, CodingKey {
-        case store = "documents"
-    }
-}
+    let documents: [Store]?
 
-struct StringValue: Codable {
-    let value: String?
-   // let integerValue: Int?
-    
-     enum CodingKeys: String, CodingKey {
-        case value = "stringValue"
-       // case integerValue
-    }
-//    enum Codingkeys: Int, CodingKey {
-//        case integerValue
-//    }
-    
-    
-    
-    
 }
-
 struct Store: Codable {
-    let name: String?
-    //let price: Int?
-    let image: String?
-    let id: String?
-     enum StoreKeys: String, CodingKey {
-        case fields
-    }
-     enum FieldKeys: String, CodingKey {
-        case name
-      //  case price
-        case image
-        case id
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: StoreKeys.self)
-       let fieldConter = try container.nestedContainer(keyedBy: FieldKeys.self, forKey: .fields)
-        name = try fieldConter.decode(StringValue.self, forKey: .name).value
-        image = try fieldConter.decode(StringValue.self, forKey: .image).value
-        id = try fieldConter.decode(StringValue.self, forKey: .id).value
-       // price = try fieldConter.decode(StringValue.self, forKey: .price).integerValue
-    }
+    let fields: Product?
 }
+struct Product: Codable {
+    let name: Name?
+    let price: Price?
+    let image: ImageUrl?
+    let id : IdString?
+}
+struct Name: Codable {
+    let stringValue: String?
+}
+struct Price: Codable {
+    let doubleValue: Double?
+}
+struct ImageUrl: Codable {
+    let stringValue: String?
+}
+struct IdString: Codable {
+    let stringValue: String?
+}
+
+
 
 
--- ShoppinList/ViewModel/HomeViewModel.swift
+++ ShoppinList/ViewModel/HomeViewModel.swift
@@ -15,21 +15,21 @@ class HomeViewModel {
     /// Function that returns total number of product
     /// - Returns: return total product
     func getTotalNumberOfProducts() -> Int {
-        let totalProduct = products?.store.count
+        let totalProduct = products?.documents?.count
         return totalProduct ?? 0
     }
     /// Function to retun  the name of Product
     /// - Parameter index: product name for each index
     /// - Returns: return only a product name as string, if does not exist then return the default as empty string.
     func getProuductName(at index: Int) -> String? {
-        let productName = products?.store.object(at: index)?.name
+        let productName = products?.documents?.object(at: index)?.fields?.name?.stringValue
         return productName ?? ""
     }
     /// Function returns an id of Product
     /// - Parameter index: based on indexpath in the row
     /// - Returns: returns the id as string
     func getProuductid(at index: Int) -> String? {
-        let productId = products?.store.object(at: index)?.id
+        let productId = products?.documents?.object(at: index)?.fields?.id?.stringValue
         return productId ?? ""
     }
     
@@ -38,8 +38,13 @@ class HomeViewModel {
     /// - Returns: returns the image url as string
     func getProductImage(at index: Int) -> String? {
         
-        let productImage = products?.store.object(at: index)?.image
+        let productImage = products?.documents?.object(at: index)?.fields?.image?.stringValue
         return productImage ?? ""
+    }
+    
+    func getPrductPrice(at index: Int) -> Double? {
+        let productPrice = products?.documents?.object(at: index)?.fields?.price?.doubleValue
+        return productPrice ?? 0.0
     }
     
     
