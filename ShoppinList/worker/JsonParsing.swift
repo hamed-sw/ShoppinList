90d84b910288a067371b47a2cf0ae3b6e2184edc
Hamed <hamedullah@gmail.com>
2021-03-02 14:36:03 +0200

    Add Delegate for communicating with other Table and gett the data and show in Home tableview

--- ShoppinList/worker/JsonParsing.swift
+++ ShoppinList/worker/JsonParsing.swift
@@ -15,12 +15,13 @@ class JsonParsing {
            if let url = URL(string: "https://firestore.googleapis.com/v1/projects/online-46aa4/databases/(default)/documents/Watches") {
             URLSession.shared.dataTask(with: url) { (data, response, error) in
                 guard let data = data else {return}
-                print(data)
+              //  print(data)
                 do {
                     let json = try JSONDecoder().decode( ProductModel.self, from: data)
                     onSucess(json)
+                   // print(json)
                 } catch {
-                    print(error.localizedDescription)
+                    print("error fond")
                     
                 }
             }.resume()
